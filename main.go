74c9f85849d2c54bfd554a80e89a550eefcce26a
Danazzz <artaputra95@gmail.com>
2024-12-23 10:11:34 +0700

    feat: add migrations

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"book-restapi/config"
 	"book-restapi/database"
 	"book-restapi/routes"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	config.LoadConfig()
+
+	database.RunMigrations()
+
 	database.Connect()
 
 	r := routes.SetupRouter()
